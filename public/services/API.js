6fe3e2b686a51b8af7a397a41a1ba78b9900b76b
Kingslayer9988 <king.slayer@tuta.io>
2025-06-16 13:13:59 +0000

    feat: Add HomePage component and template for displaying top movies

--- public/services/API.js
+++ public/services/API.js
@@ -1,11 +1,21 @@
 export const API = {
     baseURL: "/api",
     getTopMovies: async () => {
-        return API.fetch("movies/top");
+        return await API.fetch("movies/top");
     },
-    fetch: async (Service, args) => {
+    getRandomMovies: async () => {
+        return await API.fetch("movies/random");
+    },
+    getMovieById: async (id) => {
+        return await API.fetch(`movies/${id}`);
+    },
+    searchMovies: async (q, order, genre) => {
+        return await API.fetch(`movies/search/`, { q, order, genre});
+    },
+    fetch: async (serviceName, args) => {
         try {
-            const response = await fetch(API.baseURL + Service);
+            const queryString = args ? new URLSearchParams(args).toString() : "";
+            const response = await fetch(API.baseURL + serviceName + "?" + queryString);
             const result = await response.json();
             return result;
         } catch (e) {
