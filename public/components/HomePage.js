35828a4e43c9493bbb5df172181ca055539b024c
Kingslayer9988 <king.slayer@tuta.io>
2025-06-16 13:59:16 +0000

    feat: Implement MovieItemComponent and update HomePage to render movie items

--- public/components/HomePage.js
+++ public/components/HomePage.js
@@ -1,8 +1,28 @@
-export class HomePage extends HTMLElement { //homepage
+import { API } from "../services/API.js";
+
+export class HomePage extends HTMLElement { // << homepage >>
+    async render() {
+        const topMovies = await API.getTopMovies()
+        rendermoviesInList(topMovies, document.querySelector("#top-10 ul"))
+
+        const randomMovies = await API.getRandomMovies()
+        rendermoviesInList(randomMovies, document.querySelector("#random ul"))
+
+        function rendermoviesInList(movies, ul) {
+            ul.innerHTML = "";
+            movies.forEach(movie => {
+                const li = document.createElement("li");
+                li.appendChild(new MovieItemComponent(movie));
+                ul.appendChild(li);
+            });
+        }
+    }
     connectedCallback() {
         const template = document.getElementById("template-home")
         const content = template.content.cloneNode(true);
         this.appendChild(content);
+
+        this.render();
     }
 }
 
